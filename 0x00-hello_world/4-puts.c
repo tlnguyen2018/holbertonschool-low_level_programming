3901e4fbd913264f41550bb04554c6eb9a6db8ef
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-12 20:45:18 +0000

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-puts ("\"Programming is like building a multilingual puzzle");return 0
+#include <stdio.h>
+int main()
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
