f56d484f535198210fabc1d20596052bce995400
Thang Louis Nguyen <42980824+tlnguyen2018@users.noreply.github.com>
2018-09-12 15:02:21 -0700

    Update 4-puts.c
    Modified git push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
 /**
  * main - short description: write string to stdout
  *
@@ -11,7 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-=======
-
-int main() {puts("\"Programming is like building a multilingual puzzle"); return 0;}
->>>>>>> 7a001315948e17d0c1a02411812c8bb95e8b9d2a
