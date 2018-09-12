b5262be97b35135566e9a9f00116237977ce522a
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-12 21:57:34 +0000

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main()
+/**
+ * main - short description: write string to stdout
+ *
+ * Description: print out exactly the quote
+ * Return: 0
+ */
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
