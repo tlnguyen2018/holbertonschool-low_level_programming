e35ed782a0b5b8a0c707cd81e4d2061278731fac
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-14 19:14:55 +0000

    commit 11

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -16,7 +16,7 @@ int main(void)
 			putchar (a);
 			putchar (b);
 			{
-				if ((a != '9' && b != '9'))
+				if ((a != '9' || b != '9'))
 				{	putchar(',');
 					putchar(' ');
 				}
@@ -24,6 +24,6 @@ int main(void)
 		}
 	}
 
-putchar ('\n');
-return (0);
+	putchar ('\n');
+	return (0);
 }
