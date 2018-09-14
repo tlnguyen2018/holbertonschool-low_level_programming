67e937691306c7fc06569431253687b3a58e3b82
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-14 00:29:58 +0000

    commit 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - display outcome
+**Description: alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
