e008c874dc46a33c3bc446e8e1a5855add03f7e2
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-14 00:06:21 +0000

    commit 5

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - display outcome
+*Description: alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
