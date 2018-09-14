6da8425bb5f9c3db5b22770888e44e33063c5d51
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-14 17:39:44 +0000

    commit 11

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - display outcome
- *Description: combination
- *Return: 0
- */
+*main - display outcome
+*Description: combination
+*Return: 0
+*/
 int main(void)
 {
 	int a;
@@ -11,14 +11,14 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++);
 	{
-		for (b = '0', b <= '9', b++);
+		for (b = '0'; b <= '9'; b++);
 		{
 			putchar(a);
 			putchar(b);
 			if ((a != '9') && (b != '9'))
 			{
-				putchar(",");
-				putchar(" ");
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
