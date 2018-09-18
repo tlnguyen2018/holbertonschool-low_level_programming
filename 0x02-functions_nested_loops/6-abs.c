a9d5dacd463142527a8fc21decb0f48969a0344d
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-18 21:16:13 +0000

    commit abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_abs - Entry point
+ *@int : number to print
+ *Return: no return
+ */
+int _abs(int i)
+
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+		return (-i);
+}
