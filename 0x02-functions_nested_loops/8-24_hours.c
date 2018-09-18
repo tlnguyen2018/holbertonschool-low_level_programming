917b7886f09e3093dda72012c779bfccc9420a83
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-18 23:33:15 +0000

    commit times

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,17 +11,14 @@ void jack_bauer(void)
 
 	for (hour = 0 ; hour < 24 ; hour++)
 	{
-		for (mi = 0 ; mi <= 60 ; mi++)
+		for (mi = 0 ; mi < 60 ; mi++)
 		{
-			if (hour != 23 || mi != 60)
-			{
-				_putchar((hour / 10) + '0');
-				_putchar((hour % 10) + '0');
-				_putchar(':');
-				_putchar((mi / 10) + '0');
-				_putchar((mi % 10) + '0');
-				_putchar('\n');
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((mi / 10) + '0');
+			_putchar((mi % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
