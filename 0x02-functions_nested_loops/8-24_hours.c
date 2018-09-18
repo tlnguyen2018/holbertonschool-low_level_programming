e5d78db9e46fda7d27560a7a80eafd9788a70de8
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-18 23:26:32 +0000

    commit times

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - Entry point
+ */
+void jack_bauer(void)
+
+{
+	int hour;
+	int mi;
+
+	for (hour = 0 ; hour < 24 ; hour++)
+	{
+		for (mi = 0 ; mi <= 60 ; mi++)
+		{
+			if (hour != 23 || mi != 60)
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+				_putchar(':');
+				_putchar((mi / 10) + '0');
+				_putchar((mi % 10) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
