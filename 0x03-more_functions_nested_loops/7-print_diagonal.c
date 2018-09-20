6aa89611148b242a39210e33fa17a1c883d69b73
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-20 22:22:49 +0000

    commit diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_diagonal - print diagonal
+ *Description: print diagonal based on n
+ *@n: number
+ */
+void print_diagonal(int n)
+{
+	char ch = '\\';
+	char end = '\n';
+
+	while (n > 0)
+	{
+		_putchar(ch);
+		n--;
+	}
+	_putchar(end);
+}
