ce73a1635ec8d190d31dbb703d7764a2e9bc360c
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-20 20:11:58 +0000

    commit 03 print

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 
 /**
  *print_numbers - Entry point
@@ -10,8 +10,8 @@ void print_numbers(void)
 	int c;
 
 	for (c = '0' ; c <= '9' ; c++)
-		{
-			_putchar(c);
-		}
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
