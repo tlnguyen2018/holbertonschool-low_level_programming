8dd34106df93b624f2a0e835aa8361bc6d4f7205
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-24 20:03:09 +0000

    commit 0 reset

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif
