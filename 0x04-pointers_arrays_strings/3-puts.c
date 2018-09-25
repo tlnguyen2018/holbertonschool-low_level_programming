4d8e52250da94f21adbb7183a57b816704c1a0a8
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-25 00:06:19 +0000

    commit print string newline

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_puts - entry point
+ *@str: value of string
+ *Description: print out string
+ */
+
+void _puts(char *str)
+
+{
+	while(*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
