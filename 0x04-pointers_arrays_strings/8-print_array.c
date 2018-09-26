99c120b7da57a169c2721e9f119f59834699219c
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-09-26 06:04:13 +0000

    commit print out array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_array - entry point
+ *@n: number of element
+ *@a: string value
+ *Description: print out 02468
+ */
+
+void print_array(int *a, int n)
+
+{
+	int index;
+
+	for (index = 0; index < n ; index++)
+	{
+		if (index < n - 1)
+		{
+			printf("%d", a[index]);
+			putchar(',');
+		}
+		else if (index == n - 1)
+		{
+			printf("%d", a[index]);
+		}
+	}
+	putchar('\n');
+}
