5849f2442c693fd597e3c633b48f44a969b6ca59
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-02 21:06:11 +0000

    print out integer

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -6,14 +6,25 @@
 void print_number(int n)
 {
 
+	unsigned int num;
+
+	if (n < 10 && n >= 0)
+	{
+		_putchar ('0' + n);
+		return;
+	}
 	if (n < 0)
 	{
+		num = -1 * n;
 		_putchar('-');
-		n = (n * -1);
 	}
-	if (n == 0)
-		_putchar('0');
-	else if (n / 10)
+	else
+	{
+		num = n;
+	}
+	if (num >= 10)
+	{
 		print_number(n / 10);
-	_putchar (n % 10 + '0');
+	}
+	_putchar((num % 10) + '0');
 }
