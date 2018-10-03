25b1a9d2f9f7d84114f588c6ef934aac39c730ba
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-03 20:09:08 +0000

    commit recursively print string

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *_puts_recursion - entry point
+ *@s: value of character
+ *Description: print out the string
+ */
+void _puts_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
