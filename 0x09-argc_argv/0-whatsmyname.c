634a1d82e577be641dc346cc1341983322dfd791
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-08 02:07:52 +0000

    print its name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: print out the program's name
+ *@agrc: number of arguments
+ *@agrv: array of strings
+ *Return: always 0
+ */
+int main(int agrc, char *agrv[])
+{
+	int index = 0;
+
+	for (index = 0; index < agrc; index++)
+	{
+		printf("%s/n", agrv[index]);
+	}
+	return (0);
+}
