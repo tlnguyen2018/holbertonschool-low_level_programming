b93d5a572887a817a82a13c4036d67d9c9a45283
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-11 23:35:41 +0000

    commit concatenate 2 strings

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *string_nconcat - concatenate 2 strings to new allocated memory
+ *@s1: string 1
+ *@s2: string 2
+ *@n : n bytes of string 2
+ *Return: pointer that is generic with general purpose
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size1, size2;
+	int combinesize;
+	char *ptr;
+	unsigned int i, j;
+
+	if (s1 != NULL)
+	{
+		for (size1 = 0; s1[size1] != '\0'; size1++)
+			;
+	}
+	if (s2 != NULL)
+	{
+		for (size2 = 0; s2[size2] != '\0'; size2++)
+			;
+		if (size2 > n)
+			size2 = n;
+	}
+	combinesize = size1 + size2;
+	ptr = malloc(sizeof(char) * (combinesize + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < size2; j++)
+		ptr[i++] = s2[j];
+	ptr[i] = '\0';
+	return (ptr);
+}
