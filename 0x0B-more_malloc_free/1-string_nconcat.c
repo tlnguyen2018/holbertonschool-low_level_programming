d4016dc0b457f6d53fea9b6e82483a0a1511414a
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-12 18:12:56 +0000

    concate string

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -12,16 +12,17 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size1, size2;
+	unsigned int size1 = 0;
+	unsigned int size2 = 0;
 	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
-	size1 = strlen(s1);
+	size1 = _strlen(s1);
 	if (s2 == NULL)
 		s2 = "";
-	size2 = strlen(s2);
-	if (strlen(s2) <= n)
+	size2 = _strlen(s2);
+	if (_strlen(s2) <= n)
 		n = size2;
 	ptr = malloc(sizeof(char) * (size1 + n + 1));
 	if (ptr == NULL)
@@ -32,4 +33,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[size1] = s2[size2];
 	ptr[size1] = '\0';
 	return (ptr);
+}
+
+/**
+ *_strlen - string lenght
+ *@ptr : pointer
+ *Description : string lenght
+ *Return: len
+ *Description
+ */
+
+unsigned int _strlen(char *ptr)
+{
+	unsigned int i = 0;
+
+	while (ptr[i] != '\0')
+		i++;
+	return (i);
 }
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,5 +1,6 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
+unsigned int _strlen(char *ptr);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
