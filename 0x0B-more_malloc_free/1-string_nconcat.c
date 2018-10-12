3a987604f233a85ca8b19c06a3137c23c689c428
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-12 17:32:55 +0000

    concatenate 2 strings

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -32,13 +32,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			size2 = n;
 	}
 	combinesize = size1 + size2;
-	ptr = malloc(sizeof(char) * (combinesize + 1));
+	ptr = malloc(sizeof(char) * combinesize + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 		ptr[size1] = s1[size1];
-	for (size2 = 0; size2 < n; size2++)
-		ptr[size1++] = s2[size2];
+	for (size2 = 0; size2 < n; size2++, size1++)
+		ptr[size1] = s2[size2];
 	ptr[size1] = '\0';
 	return (ptr);
 }
