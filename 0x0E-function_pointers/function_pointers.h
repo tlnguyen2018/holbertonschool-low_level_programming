800fa9162fb59cc3f3988b4a1f080281bb15fee2
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-17 22:31:22 +0000

    print execute array

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,7 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-/*void array_iterator(int *array, size_t size, void (*action)(int));*/
+void array_iterator(int *array, size_t size, void (*action)(int));
 /*int int_index(int *array, int size, int (*cmp)(int));*/
 #endif
