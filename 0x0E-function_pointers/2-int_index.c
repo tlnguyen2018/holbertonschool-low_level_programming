260336d45a5e872b85b2402f1efc44cc8c15c8c6
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-18 05:09:20 +0000

    commit search for integer

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -11,16 +11,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (array == NULL)
-		return (-1);
-	if (cmp == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (size > 0 && array != '\0' && cmp != '\0')
 	{
-		if ((*cmp)(*(array + i)))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if ((*cmp)(array[i]))
+				return (i);
+		}
+	}
+	else
+	{
+		return (-1);
 	}
 	return (-1);
 }
