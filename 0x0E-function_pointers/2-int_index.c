485da90153b875ca93a32a65be10c42fea0d7aac
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-18 05:16:09 +0000

    commit match interger

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+
 /**
  *int_index - entry point
  *@array: array
@@ -7,11 +9,12 @@
  *Description: function that searches for an integer
  *Return: 1, -1, or 0
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size > 0 && array != '\0' && cmp != '\0')
+	if (size > 0 && array != 0 && cmp != 0)
 	{
 		for (i = 0; i < size; i++)
 		{
