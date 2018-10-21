514a0b3faae5555fb8e429bb1bc2c1cb4d6e8a5e
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-21 03:16:24 +0000

    print out string with variadic

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -11,18 +11,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int j;
 	char *word;
 
-	if (separator == NULL)
-		return;
 	va_start(string, n);
 	for (j = 0; j < n; j++)
 	{
 		word = va_arg(string, char *);
 		if (word == NULL)
 			printf("(nil)");
-		printf("%s", word);
-		if (separator != NULL && j != n - 1)
+		else
+			printf("%s", word);
+		if (separator != NULL && j != (n - 1))
 			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(string);
-	putchar('\n');
 }
