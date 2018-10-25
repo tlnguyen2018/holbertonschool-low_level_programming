b1a3f0bf0ff8c0ecdc0a8c1e1abb67662e150ee5
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-25 22:46:55 +0000

    print all the element, return count

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_list - print all the elements of linked list
+ *list_t - structure of the Node
+ *@h:head
+ *Description: print all the elements of linked list
+ *Return: number of elements
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d]%s\n", h->len, h->str);
+		h = h->next;
+		count += 1;
+	}
+	return (count);
+}
