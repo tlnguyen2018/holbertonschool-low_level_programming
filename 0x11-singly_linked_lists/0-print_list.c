93c656d4eab1f89d476a11c4314e9ff15b55f922
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-25 23:03:31 +0000

    modify the output from d to u

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	unsigned int count;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d]%s\n", h->len, h->str);
+			printf("[%u]%s\n", h->len, h->str);
 		h = h->next;
 		count += 1;
 	}
