dafb80e61f3c29b43376f463e33c1444df32c07f
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-10-31 20:14:42 +0000

    get the nth node

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -18,6 +18,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (count = 0; count < index; count++)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
 	if (count == index)
 		return (head);
