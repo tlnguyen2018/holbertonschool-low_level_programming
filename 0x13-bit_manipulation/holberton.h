7b7e0caa4e8b0a5491d5f93792c0e2dd47c94d6f
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-11-21 21:37:18 +0000

    update

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -2,14 +2,14 @@
 #define _HOLBERTON_H
 #include <stdio.h>
 #include <stdlib.h>
-
+int get_endianness(void);
 int _putchar(char c);
+char *decimal_to_binary(int n);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int _atoi(const char *s);
 int _pow(int x, int n);
 #endif
