d1c77f092f1e2535e5888534779b5d6d11d7f447
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-12-07 20:58:53 +0000

    print and count double linked list

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_dlistint - print the linked list and count the number of data
+ *@dlistint_t - structure of the Node
+ *@h:head
+ *Description: print all the elements of linked list and number of data
+ *Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (EXIT_FAILURE);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count = count + 1;
+	}
+	return (count);
+}
