5b52e156c47850518e6754787841eef330b3ca37
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-12-09 19:31:49 +0000

    update

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node, *tmp;
 
 	if (head == NULL)
 		return (NULL);
@@ -20,8 +20,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
+
+	tmp = *head;
+	new_node->next = tmp;
 	*head = new_node;
+	if (tmp != NULL)
+		tmp->prev = *head;
+
 	return (new_node);
 }
